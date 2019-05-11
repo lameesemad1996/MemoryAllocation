4a28fc39665bfe3d5d0eaee135a044964a27925a
Unknown <lameesemad1996@hotmail.com>
2019-05-11 03:17:57 +0200

    gui function updating

--- GUI/MemoryAllocationGUI/outputt.cpp
+++ GUI/MemoryAllocationGUI/outputt.cpp
@@ -1,5 +1,31 @@
 #include "outputt.h"
 #include "ui_outputt.h"
+#include <QWidget>
+#include <QLabel>
+#include <QLayout>
+#include <QLineEdit>
+#include <QHBoxLayout>
+#include <QtCharts/QChartView>
+#include <QtCharts/QBarCategoryAxis>
+#include <QtCharts/QValueAxis>
+#include <QtCharts/QBarSet>
+#include <QtCharts/QBarLegendMarker>
+#include <QtCharts/QStackedBarSeries>
+#include <QtCharts/QChart>
+#include <QtCharts/QLegend>
+#include <QMainWindow>
+#include <string>
+#include <iostream>
+#include <list>
+#include <iterator>
+#include <cstdlib>
+#include <QColor>
+#include <QVBoxLayout>
+#include <QDockWidget>
+#include <Qt>
+#include "Segmentt.h"
+#include "Processs.h"
+#include "Memoryy.h"
 
 Outputt::Outputt(QWidget *parent) :
     QWidget(parent),
@@ -12,3 +38,80 @@ Outputt::~Outputt()
 {
     delete ui;
 }
+
+list<Process> Outputt::getInputs()
+{
+
+}
+
+
+list<Segment> Outputt::getOP(Memory myMem)
+{
+    return myMem.memorySegmentsList;
+}
+
+void Outputt::showOP(Memory myMem)
+{
+    list<Segment> opList;
+    opList = getOP(myMem);
+
+    list<Segment>::iterator it = opList.begin();
+
+    //determining number of segments
+    unsigned long long length = opList.size();
+
+
+    //creating sets (a set for each process)
+    QtCharts::QBarSet* sets[100000];
+
+    //append sets to series
+    QtCharts::QStackedBarSeries *series = new QtCharts::QStackedBarSeries();
+
+    for(int i = 0; i < length; i++)
+    {
+        if(it->getState() == Segment::free)
+        {
+            sets[i] = new QtCharts::QBarSet(QString::fromStdString("Hole"));
+            *sets[i] << it->getLimit();
+            sets[i]->setColor(Qt::white);
+            series->append(sets[i]);
+        }
+        else
+        {
+            sets[i] = new QtCharts::QBarSet(QString::fromStdString(it->getName()));
+            *sets[i] << it->getLimit();
+            series->append(sets[i]);
+        }
+        it++;
+    }
+
+    //chart creation
+    QtCharts::QChart *chart = new QtCharts::QChart();
+    chart->addSeries(series);
+    chart->setTitle("Memory Content");
+
+    QtCharts::QValueAxis *axisY = new QtCharts::QValueAxis();
+    axisY->setTickType(QtCharts::QValueAxis::TickType::TicksDynamic);
+    axisY->setTickInterval(1);
+    axisY->setTickAnchor(0);
+    chart->addAxis(axisY, Qt::AlignBottom);
+    series->attachAxis(axisY);
+
+    //legend
+    chart->legend()->setVisible(true);
+    chart->legend()->setAlignment(Qt::AlignBottom);
+
+    //adding a view
+    QtCharts::QChartView *chartView = new QtCharts::QChartView(chart);
+    chartView->setRenderHint(QPainter::Antialiasing);
+
+
+    //set chart to be central widget and set size of chart window
+    QMainWindow* windowPtr = new QMainWindow();
+    windowPtr->setWindowModality(Qt::WindowModal);
+    windowPtr->setCentralWidget(chartView);
+    windowPtr->resize(420, 300);
+    windowPtr->show();
+
+}
+
--- GUI/MemoryAllocationGUI/outputt.h
+++ GUI/MemoryAllocationGUI/outputt.h
@@ -1,7 +1,22 @@
 #ifndef OUTPUTT_H
 #define OUTPUTT_H
 
+#include "Segmentt.h"
+#include "Processs.h"
+#include "Memoryy.h"
+#include <iostream>
+#include <string.h>
 #include <QWidget>
+#include <QLabel>
+#include <QLayout>
+#include <QLineEdit>
+#include<QHBoxLayout>
+#include <list>
+#include <iterator>
+#include <cstdlib>
+#include <QMainWindow>
+#include <QColor>
+#include <Qt>
 
 namespace Ui {
 class Outputt;
@@ -14,6 +29,10 @@ class Outputt : public QWidget
 public:
     explicit Outputt(QWidget *parent = nullptr);
     ~Outputt();
+    void static showOP(Memory myMem);
+    list<Process> static getInputs();
+    list<Segment> static getOP(Memory myMem);
+    static QMainWindow window;
 
 private:
     Ui::Outputt *ui;
